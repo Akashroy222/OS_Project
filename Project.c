d3e2694df8d0d144b15ca316dadcc4a58e22e798
Akashroy222 <36817551+Akashroy222@users.noreply.github.com>
2018-04-11 19:50:54 +0530

    Add files via upload
    in this update it take user input for
    Percentage of Replaced Page Modified and also give the result in Percentage of Page Fault Rate.

--- Project.c
+++ Project.c
@@ -13,7 +13,6 @@ double mstomis(double b)
 	return b;
 }
 
-
 int main()
 {
 double result;
@@ -21,28 +20,38 @@ double SPF;
 double RPNM;
 double MAT;
 double EAT;
+double c;
+double d;
+
 double p;
 
 
 
-printf("Enter the value of Service Page Fault (SPF):\n");
-scanf("%lf",&SPF);
-SPF=mstons(SPF);
 
-printf("Enter the value of Replace Page Not Modified(RPNM):\n");
+
+printf("Enter the value of Replace Page Not Modified(RPNM) in millisecond:\n");
 scanf("%lf",&RPNM);
 RPNM=mstons(RPNM);
 
-printf("Enter the value of Memory Access Time(MAT):\n");
+printf("Enter the Percentage of Page Replaced Modified TIME(POPRMT) :\n");
+scanf("%le",&c);
+c=c*0.01;
+RPNM=c*RPNM;
+
+printf("Enter the value of Service Page Fault (SPF) in millisecond:\n");
+scanf("%lf",&SPF);
+SPF=mstons(SPF);
+
+d=1-c;
+SPF=d*SPF;
+
+printf("Enter the value of Memory Access Time(MAT) in nanosecond:\n");
 scanf("%lf",&MAT);
-//MAT=nstos(MAT);
 
-printf("Enter the value of Effective Address Time(EAT):\n");
+printf("Enter the value of Effective Address Time(EAT) in nanosecond:\n");
 scanf("%lf",&EAT);
-//EAT=nstos(EAT);
 
-
-p=(EAT-MAT)/(0.7*RPNM+0.3*SPF-MAT);
+p=(EAT-MAT)/(RPNM+SPF-MAT);
 p=p*100;
 printf("%lf%\n",p);
 return 0;
